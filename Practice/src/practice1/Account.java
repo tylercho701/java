959f138199fb9244603c8546546bb41ffe1c82b3
tylercho701 <tylercho701@gmail.com>
2023-03-24 18:23:25 +0900

    preparation test on next mon

---
+++ Practice/src/practice1/Account.java
@@ -0,0 +1,18 @@
+package practice1;
+
+public class Account {
+	private String anu;
+	private String aon;
+	private int money;
+	public Account(String anu, String aon, int money) {
+	 this.anu = anu;
+	 this.aon = aon;
+	 this.money = money;
+	}
+	public String getAnu() { return anu; }
+	public void setAnu(String anu) { this.anu = anu; }
+	public String getAon() { return aon; }
+	public void setAon(String aon) { this.aon = aon; }
+	public int getMoney() { return money; }
+	public void setMoney(int money) { this.money = money; }
+}
